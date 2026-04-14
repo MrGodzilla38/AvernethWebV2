b4c141b44a4e9f2309d98f8b3f7b8bbb73bcfaa7
MrGodzilla38 <oyunustasigodzilla@gmail.com>
2026-04-14 20:29:04 +0300

    son çırpınışlar sorunu hala çözemedim

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -68,9 +68,9 @@ export function generateToken(username: string): string {
 
 export function setAuthCookie(response: any, token: string) {
   response.cookies.set("averneth_session", token, {
-    httpOnly: true,
+    httpOnly: false,
     secure: false, //process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: JWT_EXPIRES_DAYS * 24 * 60 * 60,
     path: "/",
   });
@@ -78,9 +78,9 @@ export function setAuthCookie(response: any, token: string) {
 
 export function clearAuthCookie(response: any) {
   response.cookies.set("averneth_session", "", {
-    httpOnly: true,
+    httpOnly: false,
     secure: false, //process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: 0,
     path: "/",
   });
