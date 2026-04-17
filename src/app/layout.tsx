49c433614b9fdd5271d3b2ccbcd43a2eec0ad086
MrGodzilla38 <oyunustasigodzilla@gmail.com>
2026-04-17 14:17:57 +0300

    Siteye favicon eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Averneth Web',
   description: 'Averneth Web Application',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
